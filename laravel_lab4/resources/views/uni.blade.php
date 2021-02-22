39b88c6ca44d60dfdfc4d68bee99518a7bf5361b
Beka <190103086@stu.sdu.edu.kz>
2021-02-22 11:29:50 +0600

    3

---
+++ laravel_lab4/resources/views/uni.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Suleyman Demirel University</h1>
+    <img src="https://studykazakhstan.com/uploads/images/universities/1538557576uQj843kE.jpg">
+</body>
+</html>
